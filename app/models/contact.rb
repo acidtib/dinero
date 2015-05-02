8c831d5b6ef31aae901aabc865a5a80084c6c5d6
Dan Vera <dan@yovu.co>
2015-05-02 10:47:25 -0400

    some dev gems

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,16 @@
+# == Schema Information
+#
+# Table name: contacts
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer
+#  name       :string
+#  phone      :string
+#  email      :string
+#  spei       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Contact < ActiveRecord::Base
 end
--- test/models/contact_test.rb
+++ test/models/contact_test.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: contacts
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer
+#  name       :string
+#  phone      :string
+#  email      :string
+#  spei       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class ContactTest < ActiveSupport::TestCase
