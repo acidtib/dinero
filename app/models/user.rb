a4c981acd8fd6731442ab2664c64f151ffe92b14
Dan Vera <dan@yovu.co>
2015-05-02 11:10:54 -0400

    validate presence of name

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,5 +30,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :contacts
+  has_many :contacts, dependent: :destroy
+  
+  validates :name, presence: true
 end
