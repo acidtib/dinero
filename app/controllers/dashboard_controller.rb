67afcd64dc7ad64214125237d37af1fc5116e692
Dan Vera <dan@yovu.co>
2015-05-02 15:53:02 -0400

    get contacts on dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,8 +1,16 @@
 class DashboardController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_user, only: [:door, :settings]
+
   def door
+    @contacts = @user.contacts.all
   end
 
   def settings
   end
+
+  private
+    def set_user
+      @user = User.find(current_user.id)
+    end
 end
